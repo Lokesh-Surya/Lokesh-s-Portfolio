b93d5230b2b1631897ff97fc4c5eb4c8426f6609
Lokesh-Surya <91713027+Lokesh-Surya@users.noreply.github.com>
2025-05-30 17:11:03 +0530

    h

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -41,7 +41,7 @@ const About = () => {
             
             <div className="space-y-6 text-gray-700">
               <p className="text-lg leading-relaxed">
-                I am a <span className="font-semibold text-primary">Digital Transformation Engineer</span> with extensive experience working with leading global  <span className="font-semibold text-primary">energy, oil & gas, and chemical companies</span> across North America, Europe, and India to spearhead multi-site digital initiatives that enhance operational resilience and asset performance.
+                I am a <span className="font-semibold text-primary">Digital Transformation Engineer</span> with extensive experience working with leading global  <span className="font-semibold">energy, oil & gas, and chemical companies</span> across<span className="font-semibold">North America, Europe, and India</span> to spearhead multi-site digital initiatives that enhance operational resilience and asset performance.
               </p>
               
               <div className="border-l-4 border-primary pl-6 py-2">
@@ -60,7 +60,7 @@ const About = () => {
               
               <div className="flex items-center space-x-3 pl-4 border-l-4 border-blue-300">
                 <p className="text-lg font-medium">
-                  Skilled and certified in <span className="font-semibold text-primary">Project Management</span>I excel at coordinating resources, mitigating risks, and managing timelines and budgets to deliver projects on time and within scope, while maintaining high-quality standards.
+                  Skilled and certified in <span className="font-semibold text-primary">Project Management</span>, I excel at coordinating resources, mitigating risks, and managing timelines and budgets to deliver projects <span className="font-semibold">on time and within scope</span>, while maintaining high-quality standards.
                 </p>
               </div>
               
