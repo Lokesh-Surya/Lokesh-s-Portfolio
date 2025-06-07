f1a1af248e5b7e3b9d8883c8aa6191a5dce9a49b
Lokesh-Surya <91713027+Lokesh-Surya@users.noreply.github.com>
2025-06-07 07:44:08 +0000

    f

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,6 +5,7 @@ import About from "../components/About";
 import Innovation from "../components/Innovation";
 import Certifications from "../components/Certifications";
 import Skills from "../components/Skills";
+import Testimonials from "../components/Testimonials";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 
@@ -17,6 +18,7 @@ const Index = () => {
       <Innovation />
       <Certifications />
       <Skills />
+      <Testimonials />
       <Contact />
       <Footer />
     </div>
